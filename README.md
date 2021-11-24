6901fb9991c0ce2d5d2274c71cd69857f3beeeb8
bietdoikiem <minhlucky2408@gmail.com>
2021-11-24 13:56:01 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# COSC2658 Group Assignment
---
+++ src/main/java/com/RMIT/algorithm/Main.java
@@ -0,0 +1,7 @@
+package com.RMIT.algorithm;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Welcome to Group assignment of COSC2658 course");
+  }
+}
