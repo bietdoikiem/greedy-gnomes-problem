24da7b515f1cfc7bb1fe7d2588ddef571c04a709
thien <s3817852@rmit.edu.vn>
2021-11-27 21:40:30 +0700

    Adding read file function

--- src/main/java/com/RMIT/algorithm/DPTopDown.java
+++ src/main/java/com/RMIT/algorithm/DPTopDown.java
@@ -1,24 +1,14 @@
 package com.RMIT.algorithm;
 
 import java.util.ArrayList;
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
 
 public class DPTopDown {
         // Number of rows, columns and steps
-        static int n_rows=12, n_columns=23, step=0;
+        static int n_rows=0, n_columns=0, step=0;
         static ArrayList<Character> path = new ArrayList<Character>();
-
-        static int a[][] = {{0, 'X', 0, 0, 'X', 0, 0, 0, 0, 0, 0, 0, 0, 0, 'X', 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0},
-                {2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
-                {0, 'X', 4, 0, 0, 0, 0, 0, 0, 0, 0, 'X', 0, 'X', 0, 0, 0, 1, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 'X', 0, 2, 0, 0, 0, 0, 0},
-                {0, 0, 0, 'X', 0, 0, 0, 0, 0, 0, 'X', 0, 0, 0, 0, 0, 0, 2, 'X', 0, 0, 0, 0},
-                {2, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 'X', 4, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-                {0, 0, 0, 'X', 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0},
-                {1, 0, 0, 'X', 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 3, 0, 0, 'X', 0, 0},
-                {2, 'X', 'X', 'X', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'X', 0, 0, 0, 0, 0, 'X', 0}};
 
         // Variable temp_max_sum is used to store maximum sum till current position
         static int temp_max_sum[][] = new int[27][27];
@@ -35,18 +25,40 @@ public class DPTopDown {
         // For continuous update of maximum sum required
         static int total_sum = 0;
 
+        static int[][] read_sample_file() throws FileNotFoundException{
+            
+                File myObj = new File("/Users/thien/Desktop/cosc2658-group-asm/src/main/java/resources/sample1.txt");
+                Scanner myReader = new Scanner(myObj);
+                n_rows = Integer.parseInt(myReader.next());
+                n_columns = Integer.parseInt(myReader.nextLine().strip());
+                int i=0,j=0;
+                int[][] input_array = new int[n_rows][n_columns];
+                while (myReader.hasNextLine()) {
+                        if (j == n_columns){j =0; i++;}
+                        String data = myReader.next();
+                    if (data.charAt(0) == 'x') input_array[i][j] = -1;
+                    else if (data.charAt(0) == '.') input_array[i][j] = 0;
+                    else input_array[i][j] = Integer.parseInt(data);
+                    
+                    j++;
+                }
+                myReader.close();
+              return input_array;
+        }
+
         static void print_array(int[][] arr){
-            for (int[] ints : arr) {
-                for (int anInt : ints) {
-                    if (anInt == 0) {
+            for (int i = 0; i < n_rows; i++) {
+                for (int j = 0; j < n_columns; j++) {
+                    if (arr[i][j] == 0) {
                         System.out.print("XX ");
-                    } else if (anInt < 10)
-                        System.out.print(anInt + "  ");
+                    } else if (arr[i][j] < 10)
+                        System.out.print(arr[i][j] + "  ");
                     else
-                        System.out.print(anInt + " ");
+                        System.out.print(arr[i][j] + " ");
                 }
                 System.out.println();
             }
+            
         }
         static int find_path(int row, int column){
             // Base case
@@ -67,14 +79,14 @@ public class DPTopDown {
             return step;
         }
         // Function to calculate maximum sum of path
-        static int maximum_sum_path(int row, int column)
+        static int maximum_sum_path(int row, int column,int[][] input_matrix)
         {
             // Checking boundary condition
-            if (row == n_rows - 1 && column == n_columns - 1 && a[row][column] != 'X')
-                return a[row][column];
+            if (row == n_rows - 1 && column == n_columns - 1 && input_matrix[row][column] != -1)
+                return input_matrix[row][column];
 
             // Checking whether or not a(row, column) is visited
-            if (visited[row][column] != 0 && a[row][column] != 'X')
+            if (visited[row][column] != 0 && input_matrix[row][column] != -1)
                 return temp_max_sum[row][column];
 
             // Marking a(row, column) is visited
@@ -85,26 +97,27 @@ public class DPTopDown {
             // Checking whether the position hasn't visited the last row or the last column. Making recursive call
             // for all the possible moves from the current cell and then adding the maximum returned by
             // the calls and updating it.
-            if (row < n_rows - 1 && column < n_columns - 1 && a[row][column] != 'X')
+            if (row < n_rows - 1 && column < n_columns - 1 && input_matrix[row][column] != -1)
             {
                 int current_sum = Math.max(
-                        maximum_sum_path(row, column + 1),
-                        maximum_sum_path(row + 1, column));
-                total_sum = a[row][column] + current_sum;
+                        maximum_sum_path(row, column + 1,input_matrix),
+                        maximum_sum_path(row + 1, column,input_matrix));
+                total_sum = input_matrix[row][column] + current_sum;
+                
             }
 
             // Checking whether position
             // has reached last row
-            else if (row == n_rows - 1 & a[row][column] != 'X')
-                total_sum = a[row][column] +
-                        maximum_sum_path(row, column + 1);
+            else if (row == n_rows - 1 & input_matrix[row][column] != -1){
+                    total_sum = input_matrix[row][column] + maximum_sum_path(row, column + 1,input_matrix); 
+            }
+                
 
                 // If the position is in the last column
-            else
-                if (a[row][column] != 'X')
-                total_sum = a[row][column] +
-                        maximum_sum_path(row + 1, column);
-
+            else if (input_matrix[row][column] != -1 ){
+                total_sum = input_matrix[row][column] + maximum_sum_path(row+1, column,input_matrix);
+            }
+             
             // Updating the maximum sum till the current position in the temp_max_sum
             temp_max_sum[row][column] = total_sum;
 
@@ -115,13 +128,17 @@ public class DPTopDown {
         // Driver Code
         public static void main(String[] args)
         {
-//             Calling the implemented function
-            System.out.println( "Gold: "+maximum_sum_path(0, 0));
-            System.out.println("Steps: "+find_path(0,0));
-            System.out.println("Path: "+ path);
-//            print_array(temp_max_sum);
-//            System.out.println("--------------");
-//            print_array(path_arr);
+            try {
+                int[][] a = read_sample_file();
+                //             Calling the implemented function
+                System.out.println( "Gold: "+maximum_sum_path(0, 0,a));
+                System.out.println("Steps: "+find_path(0,0));
+                System.out.println("Path: "+ path);
+                // print_array(path_arr);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+           
         }
     }
 
