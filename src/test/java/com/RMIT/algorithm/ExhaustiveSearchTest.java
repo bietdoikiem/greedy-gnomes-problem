49b5cd6dd13055ca915884a96e4cccfe0fed3b52
bietdoikiem <minhlucky2408@gmail.com>
2021-12-15 11:36:51 +0700

    feat: add 9x16 matrix test case

--- src/test/java/com/RMIT/algorithm/ExhaustiveSearchTest.java
+++ src/test/java/com/RMIT/algorithm/ExhaustiveSearchTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 
 public class ExhaustiveSearchTest {
   long startTime;
+  ExhaustiveSearch.OptimalSolution solution;
 
   @Before
   public void start() {
@@ -24,15 +25,26 @@ public class ExhaustiveSearchTest {
 
   @Test
   public void solve_ShouldReturn6GoldAnd2Steps_WhenGiven3x3Matrix() {
-    ExhaustiveSearch.OptimalSolution solution = ExhaustiveSearch
+    System.out.println("Test case: 3x3 Matrix");
+    solution = ExhaustiveSearch
         .solve(MatrixUtils.parseFromFile("src/test/resources/sample-3x3.txt"));
     assertEquals(6, solution.gold);
     assertEquals(2, solution.steps);
   }
 
   @Test
+  public void solve_ShouldReturn27GoldAnd19Steps_WhenGiven9x16Matrix() {
+    System.out.println("Test case: 9x16 Matrix");
+    solution = ExhaustiveSearch
+        .solve(MatrixUtils.parseFromFile("src/test/resources/sample-9x16.txt"));
+    assertEquals(27, solution.gold);
+    assertEquals(19, solution.steps);
+  }
+
+  @Test
   public void solve_ShouldReturn18GoldAnd27Steps_WhenGiven12x23Matrix() {
-    ExhaustiveSearch.OptimalSolution solution = ExhaustiveSearch
+    System.out.println("Test case: 12x23 Matrix");
+    solution = ExhaustiveSearch
         .solve(MatrixUtils.parseFromFile("src/test/resources/sample-12x23.txt"));
     assertEquals(18, solution.gold);
     assertEquals(27, solution.steps);
