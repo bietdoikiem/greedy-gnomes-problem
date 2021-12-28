720bd3de97b82d8f8033a36b80efc509733877b5
bietdoikiem <minhlucky2408@gmail.com>
2021-12-28 11:24:36 +0700

    fix: fix argument of CLI

--- src/main/java/com/RMIT/algorithm/Main.java
+++ src/main/java/com/RMIT/algorithm/Main.java
@@ -4,13 +4,12 @@ import java.io.FileNotFoundException;
 
 import com.RMIT.algorithm.utils.MenuUtils;
 
-
 public class Main {
-  
+
   public static void main(String[] args) throws FileNotFoundException {
-  
+
     // src/main/resources/sample-12x23.txt
     System.out.println("===== Welcome to the Greedy Gnomes Problem program =====");
-    MenuUtils.start("src/main/resources/sample-12x23.txt");
+    MenuUtils.start(args[0]);
   }
 }
